52804c24c8653b004f644f99a28653c46ed37339
athuljain <athuljainkj2@gmail.com>
2025-02-12 08:19:55 +0530

    update resume

--- src/Components/Header.jsx
+++ src/Components/Header.jsx
@@ -5,10 +5,8 @@ import {AiFillInstagram} from "react-icons/ai"
 import "./Header.css";
 import { AiFillCodeSandboxCircle } from "react-icons/ai";
 
-//import AthulJainResumePDF from "../Assets/AthulJainResume.pdf";
-// import AthulJainResume from "../Assets/AthulJain.pdf"
-// import AthulJainMern from '../Assets/AthulJainMern.pdf'
-import AthulJainMern from '../Assets/AthulJainCv2024.pdf'
+import AthulJainMern from '../Assets/athuljainMernStack.pdf'
+// import AthulJainMern from '../Assets/AthulJainCv2024.pdf'
 const Header = () => {
   return (
     <header id="home">
