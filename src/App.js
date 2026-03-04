ace7aa6d8209ff1c8a71a3f14faa3b01f8b885dd
athuljain <athuljainkj2@gmail.com>
2026-03-04 10:43:50 +0530

    contact page

--- src/App.js
+++ src/App.js
@@ -1,13 +1,11 @@
-//import logo from './logo.svg';
+
 import "./App.css";
 import Header from "./Components/Header";
 import Footer from "./Components/Footer";
 import Topbar from "./Components/Topbar";
 import Intro from "./Components/Intro";
 import Experience from "./Components/Experience";
-
 import Contact from "./Components/Contact";
-// import Portfolio from "./Components/Portfolio";
 import Portfolio from "./Components/PortfolioPage";
 
 function App() {
